0434ca564fb979eace160fb8fbf6f7a8e3aaf0a0
Low Chee Meng <lowcheemeng02@gmail.com>
2023-10-31 13:25:14 +0800

    restore s3 backend

--- provider.tf
+++ provider.tf
@@ -7,13 +7,11 @@ terraform {
     }
   }
 
-  # backend "s3" {
-  #   bucket = "sctp-ce3-tfstate-bucket-1"
-  #   key    = "friends.tfstate"
-  #   region = "us-east-1"
-  # }
-
-
+  backend "s3" {
+    bucket = "sctp-ce3-tfstate-bucket-1"
+    key    = "friends-ce-3-group-capstone-infra.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
